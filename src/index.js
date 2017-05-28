856b23374ca20e12dee37be032656e723828489e
Seungwoo Lee <outtoin@kaist.ac.kr>
2017-05-29 00:32:08 +0900

    ref

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './components/App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-const rootElement = document.getElementById('root')
-ReactDOM.render(<App />, rootElement)
+class Hello extends React.Component {
+    handleClick() {
+        this.input.value = "";
+        this.input.focus();
+    }
+
+    render() {
+        return (
+            <div>
+                <input ref={ref => this.input = ref} />
+                <button onClick={this.handleClick.bind(this)}>Click Me!</button>
+            </div>
+        );
+    }
+}
+
+//const rootElement = document.getElementById("root");
+ReactDOM.render(<Hello />, document.getElementById("app"));
